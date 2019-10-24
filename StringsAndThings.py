70271e19e2b413fbb37af46c67a899407b8f602a
Rocky108 <dougald2001@gmail.com>
2019-10-24 08:35:35 -0500

    2

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -1,2 +1,27 @@
 # strings
 #anything that falls withing""
+
+#concatenation
+# put 2 or  more strings together
+
+firstName= "Fred"
+lastName="Flintstone"
+
+print(firstName+ " "+ lastName)
+
+fullName = firstName + " " +lastName
+
+print(fullName)
+
+# repititon
+#repitition operator: *
+
+print("Hip "*2 + "Hooray!")
+
+def rowYourBoat():
+    print("Row, "*3 + "your boat")
+    print("gently down the stream")
+    print("Merrily, "*4)
+    print("Life is but a dream")
+
+rowYourBoat()
